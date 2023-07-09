b5efdd9a232c0b49f35ad3d5480b1c4e5b3fcb95
Maoyue <95519633+MagicTeaMC@users.noreply.github.com>
2023-07-09 23:13:56 +0800

    1.0.0

--- src/main/java/org/milkteamc/hotspring/HotSpring.java
+++ src/main/java/org/milkteamc/hotspring/HotSpring.java
@@ -1,17 +1,167 @@
 package org.milkteamc.hotspring;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public final class HotSpring extends JavaPlugin {
+public class HotSpring extends JavaPlugin implements Listener {
+    private FileConfiguration config;
+    private Location hotSpring1;
+    private Location hotSpring2;
+    private int intervalSeconds;
+    private int coinAmount;
+    private Economy economy;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
+        loadConfig();
+        setupEconomy();
+        startTimer();
+        getLogger().info("溫泉插件已經啟用");
+        getLogger().info("作者：Maoyue 為奶茶伺服器製作");
+        getLogger().info("支援群組：https://discord.gg/uQ4UXANnP2");
+    }
 
+    private void loadConfig() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        config = getConfig();
+        hotSpring1 = getLocationFromConfig("HotSpring1");
+        hotSpring2 = getLocationFromConfig("HotSpring2");
+        intervalSeconds = config.getInt("IntervalSeconds", 5);
+        coinAmount = config.getInt("CoinAmount", 1);
+    }
+
+    private void setupEconomy() {
+        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
+            getLogger().warning("請先安裝 Vault 才能使用本插件");
+            return;
+        }
+
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            getLogger().warning("請先安裝任意經濟插件才能使用本指令");
+            return;
+        }
+
+        economy = rsp.getProvider();
+        getLogger().info("已成功連接到經濟插件：" + rsp.getProvider().getName());
+    }
+
+    private Location getLocationFromConfig(String path) {
+        World world = Bukkit.getWorld(config.getString(path + ".World"));
+        double x = config.getDouble(path + ".X");
+        double y = config.getDouble(path + ".Y");
+        double z = config.getDouble(path + ".Z");
+        float yaw = (float) config.getDouble(path + ".Yaw");
+        float pitch = (float) config.getDouble(path + ".Pitch");
+        return new Location(world, x, y, z, yaw, pitch);
+    }
+
+    private void startTimer() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if (isInHotSpring(player)) {
+                        int seconds = intervalSeconds;
+                        String subtitle = ChatColor.AQUA + "在温泉中，每 " + seconds + " 秒可以獲得" + coinAmount + "個金幣！";
+                        sendSubtitle(player, subtitle);
+                        giveCoins(player, coinAmount);
+                    }
+                }
+            }
+        }.runTaskTimer(this, 0, intervalSeconds * 20);
+    }
+
+    private boolean isInHotSpring(Player player) {
+        Location playerLocation = player.getLocation();
+        return (playerLocation.getWorld().equals(hotSpring1.getWorld()) &&
+                isInsideRegion(playerLocation, hotSpring1, hotSpring2) &&
+                playerLocation.getBlock().isLiquid());
+    }
+
+    private boolean isInsideRegion(Location location, Location pos1, Location pos2) {
+        double x1 = Math.min(pos1.getX(), pos2.getX());
+        double y1 = Math.min(pos1.getY(), pos2.getY());
+        double z1 = Math.min(pos1.getZ(), pos2.getZ());
+        double x2 = Math.max(pos1.getX(), pos2.getX());
+        double y2 = Math.max(pos1.getY(), pos2.getY());
+        double z2 = Math.max(pos1.getZ(), pos2.getZ());
+        double x = location.getX();
+        double y = location.getY();
+        double z = location.getZ();
+        return (x >= x1 && x <= x2 && y >= y1 && y <= y2 && z >= z1 && z <= z2);
+    }
+
+    private void sendSubtitle(Player player, String subtitle) {
+        player.sendTitle("", subtitle, 0, 60, 20);
+    }
+
+    private void giveCoins(Player player, double amount) {
+        if (economy != null) {
+            economy.depositPlayer(player, amount);
+        }
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("hs1")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                hotSpring1 = player.getLocation();
+                saveLocationToConfig("HotSpring1", hotSpring1);
+                sender.sendMessage(ChatColor.GREEN + "第一點設定完成");
+            } else {
+                sender.sendMessage("只有玩家可使用此指令");
+            }
+            return true;
+        } else if (command.getName().equalsIgnoreCase("hs2")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                hotSpring2 = player.getLocation();
+                saveLocationToConfig("HotSpring2", hotSpring2);
+                sender.sendMessage(ChatColor.GREEN + "第二點設定完成");
+            } else {
+                sender.sendMessage("只有玩家可使用此指令");
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void saveLocationToConfig(String path, Location location) {
+        config.set(path + ".World", location.getWorld().getName());
+        config.set(path + ".X", location.getX());
+        config.set(path + ".Y", location.getY());
+        config.set(path + ".Z", location.getZ());
+        config.set(path + ".Yaw", location.getYaw());
+        config.set(path + ".Pitch", location.getPitch());
+        saveConfig();
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (isInHotSpring(player)) {
+            int seconds = intervalSeconds;
+            String message = ChatColor.AQUA + "在温泉中，每 " + seconds + " 秒可以獲得" + coinAmount + "個金幣！";
+            sendSubtitle(player, message);
+            giveCoins(player, coinAmount);
+        }
     }
 }
